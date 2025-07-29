b41f874ce2acfe0e3a542394356e05d2a616bbc5
vasanth <smailyvasanth@gmial.com>
2025-07-29 15:19:21 +0530

    files

--- client/src/components/CourseCard.jsx
+++ client/src/components/CourseCard.jsx
@@ -2,23 +2,24 @@ import { Link } from 'react-router-dom';
 
 function CourseCard({ course, onDelete }) {
   return (
-    <div className="border p-4 rounded shadow bg-white w-64 text-black">
-      <h2 className="text-lg font-bold mb-2">{course.courseName}</h2>
-      <p className="mb-1">Course Code: {course.courseCode}</p>
-      <p className="mb-1">Instructor: {course.instructor}</p>
-      <p>Credits: {course.credits}</p>
+    <div className="bg-white shadow-md rounded-xl p-4 text-gray-800">
+      <h2 className="text-xl font-semibold">Name:{course.courseName}</h2>
+      <p ><strong>Course Code: </strong>{course.courseCode}</p>
+      <p ><strong>Instructor :</strong> {course.instructor}</p>
+      <p><strong> Credits    : </strong> {course.credits}</p>
       
       <div className='mt-4 flex justify-between'>
         <Link
-          to={`/edit/${course.id}`}
-          className="text-blue-600 hover:text-blue-800 font-medium transition-colors"
+          onClick={() => navigate(`/edit/${course.id}`)}
+          className="bg-yellow-500 hover:bg-yellow-600 text-white font-medium px-4 py-2 rounded shadow"
+
         >
           Edit
         </Link>
 
         <button
           onClick={() => onDelete(course.id)}
-          className="text-red-600 hover:text-red-800 font-medium"
+          className=" text-white px-3 py-1 rounded bg-red-600"
         >
           Delete
         </button>
